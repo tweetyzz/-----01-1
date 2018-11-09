46025123b64c668d050dc2f03260e2d875ea4081
tweety <ekak9380@gmail.com>
2018-11-09 15:04:07 +0900

    빙고게임 01-1

---
+++ 01-1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+
+void initiate_bingo();
+void count_bingo();
+void process_bingo();
+
+int initiate_bingo[n][n];
+
+int main(int argc, char *argv[]) {
+	
+	return 0;
+}
